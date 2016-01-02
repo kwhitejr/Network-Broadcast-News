d2e9791532612326f0fcdfb6af1d0e4105100b02
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-02 11:21:04 -1000

    server and client can hear each other

--- client.js
+++ client.js
@@ -2,10 +2,13 @@ var net = require('net');
 var PORT = 8080;
 var socket = new net.Socket();
 
+// Setting up the listener
 socket.on('connect', function () {
     console.log('Connected to server on port ' + PORT);
     socket.write('\'cause I wonder where you are...');
 });
 
+// When connected...
 socket.connect(PORT, function () {
+  console.log('I am so connected.');
 });
