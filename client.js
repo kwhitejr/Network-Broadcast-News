c3c302a4631f26331c448d6e6e277648cdba2786
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-02 15:52:34 -1000

    the ADMIN lives

--- client.js
+++ client.js
@@ -1,14 +1,50 @@
 var net = require('net');
 var PORT = 8080;
 var socket = new net.Socket();
+var fs = require('fs');
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+var username = '';
+socket.setEncoding('utf8');
 
-// Setting up the listener
-socket.on('connect', function () {
+
+getUsername();
+
+// STEP 1
+function getUsername () {
+  rl.question('Enter a username: ', function (answer) {
+    console.log('Thank you, ', answer);
+    username += answer;
+    rl.close();
+    // Readline is weird. Avoid it.
+    process.stdin.resume();
+    connectToServer();
+  });
+}
+
+// STEP 2
+function connectToServer () {
+  // Setting up the listener
+  socket.on('connect', function () {
+    // Client-side local port will be server-side remote port.
     console.log('Connected to server on port ' + PORT);
-    socket.write('\'cause I wonder where you are...');
-});
+  });
 
-// When connected...
-socket.connect(PORT, function () {
-  console.log('I am so connected.');
-});
+  // When connected...
+  socket.connect(PORT, function () {
+    console.log('Welcome, ' + username + ', you are ready to write. Your remote port is ' + socket.localPort);
+
+    // Function that will pipe input to server.
+    process.stdin.on('data', function (data) {
+      socket.write(username + ': ' + data);
+    });
+
+    // What to do on receipt of data.
+    socket.on('data', function (data) {
+      process.stdout.write(data);
+    });
+  });
+}
--- server.js
+++ server.js
@@ -1,21 +1,52 @@
 var net = require('net');
+var fs = require('fs');
 var PORT = 8080;
 var server = net.createServer(connectionListener);
+var idCounter = 0;
+var connectedUsers = [];
 
+// this is all a 'server.on('connection')' event
 function connectionListener (socket) {
-  console.log('Client connected.');
-  console.log(typeof(socket));
-
   socket.setEncoding('utf8');
 
+  console.log(socket.remotePort + ' is connected.');
+  server.getConnections(function (err, count) {
+    console.log("There are now this many connections: ", count);
+  });
+
+  //assign connected user to object
+  connectedUsers.push(socket);
+
+  // What to do with data received. Socket stream is R/W
   socket.on('data', function (data) {
-    console.log(data);
+    // data received from client (process.stdin) is written (stdout) to server
+    process.stdout.write(data);
+    // same data is distributed to each other client ('client' is a socket)
+    connectedUsers.forEach(function(client) {
+      client.write(data);
+    });
+  });
+
+
+  // delete user from connected user object on socket close
+  socket.on('close', function (socket) {
+    connectedUsers.splice(connectedUsers.indexOf(socket), 1);
+    connectedUsers.forEach(function (user) {
+      console.log(user._peername.port);
+    });
   });
 }
 
 // server.on('connection', function (socket) {
 //   console.log('\'cause I wonder where you are...');
 // });
+
+// Function that will pipe input to server.
+process.stdin.on('data', function (data) {
+  connectedUsers.forEach(function(client) {
+    client.write('[ADMIN]: ' + data);
+  });
+});
 
 server.listen(PORT, function () {
   console.log('Hello... is it ' + PORT + ' you\'re looking for?');
